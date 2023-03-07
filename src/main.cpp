62b5196589107f2279bd134df672de9a53c98765
Ni Sel <nselyavin@inbox.ru>
2023-03-07 15:03:00 +0300

    feat: libpqxx added

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,17 @@
 #include <csearch/Test.h>
 #include <ranges>
 #include <thread>
+#include <pqxx/pqxx>
 
 int main() {
 
     Test::hello();
+    try{
+        pqxx::connection conn("dbname=iihero hostaddr=127.0.01 user=foo password=foo1");
+
+    } catch (std::exception& e){
+        std::cout << "Failed connect to pgsql" << e.what();
+    }
 
 
     return 0;
