520345aaecf0c5cc1232d311fdc215cba776b39f
Nikia Selyavin <nselyavin@inbox.ru>
2023-05-10 16:30:48 +0300

    feat: protoc from local installed grpc

--- CompileProtos.sh
+++ CompileProtos.sh
@@ -1,12 +1,13 @@
  #!/bin/bash
 
 echo "Start compiling process"
-echo `protoc --version`
+echo `./build/external/grpc/third_party/protobuf/protoc --version`
 
 grpc_plugin_PATH=$(dirname "$0")/build/external/grpc/grpc_cpp_plugin
 protos_path=$(dirname "$0")/protos
 protos_headers_path=$(dirname "$0")/protos-gen
 protos_sources_path=$(dirname "$0")/protos-gen
+protoc=./build/external/grpc/third_party/protobuf/protoc
 
 echo "grpc_plugin_PATH $grpc_plugin_PATH"
 
@@ -16,8 +17,8 @@ mkdir -p $protos_sources_path
 for protos_file in "$protos_path"/*
 do
     echo Compile: $protos_file
-    protoc -I $protos_path --grpc_out=$protos_headers_path --plugin=protoc-gen-grpc=$grpc_plugin_PATH $protos_file
-    protoc -I $protos_path --cpp_out=$protos_headers_path $protos_file
+    $protoc -I $protos_path --grpc_out=$protos_headers_path --plugin=protoc-gen-grpc=$grpc_plugin_PATH $protos_file
+    $protoc -I $protos_path --cpp_out=$protos_headers_path $protos_file
 done
 
 echo "Compiling is done"
