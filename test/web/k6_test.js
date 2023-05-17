bcd238f5235d8e97b4a57467d6885ae387c0bf1f
Nikita Selyavin <nselyavin@inbox.ru>
2023-05-17 12:17:41 +0300

    test: k6 test for gRPC testing

--- test/web/k6_test.js
+++ test/web/k6_test.js
@@ -1,5 +1,5 @@
 import grpc from 'k6/net/grpc';
-import { check, sleep } from 'k6';
+import {check, sleep} from 'k6';
 
 const client = new grpc.Client();
 client.load(['/home/fumedesk/projects/cpp/anezkasearch/protos'], 'AnezkaSearch.proto');
@@ -7,23 +7,12 @@ client.load(['/home/fumedesk/projects/cpp/anezkasearch/protos'], 'AnezkaSearch.p
 export default () => {
     client.connect('0.0.0.0:50051', {
         plaintext: true,
-        reflect:false,
-        timeout:2000
+        reflect: false,
+        timeout: 2000
     });
 
     const data_1 = {
-        "indexes": [
-            {
-                "s_ind": "book_1"
-            },
-            {
-                "s_ind": "book_2"
-            },
-            {
-                "s_ind": "book_3"
-            }
-        ],
-        "key": "worda"
+        "indexes": [{"s_ind": "book_1"}, {"s_ind": "book_2"}, {"s_ind": "book_3"}], "key": "worda"
     };
 
     const response_1 = client.invoke('anezkasearch.IndexStorageService/InsertIndex', data_1);
@@ -32,27 +21,19 @@ export default () => {
     });
     console.log(response_1.message);
 
-    const data_2 = { "key": "worda" };
+    const data_2 = {"key": "worda"};
     const response_2 = client.invoke('anezkasearch.IndexStorageService/GetIndexes', data_2);
     check(response_2, {
         'status is OK 2': (r) => r && r.status === grpc.StatusOK,
         'answer is correct 2':
             response_2.message['indexes'][0]["sInd"] === "book_1"
-            &&  response_2.message['indexes'][1]["sInd"] === "book_2"
-            &&  response_2.message['indexes'][2]["sInd"] === "book_3",
+            && response_2.message['indexes'][1]["sInd"] === "book_2"
+            && response_2.message['indexes'][2]["sInd"] === "book_3",
     });
     console.log(response_2.message);
 
     const data_3 = {
-        "indexes": [
-            {
-                "s_ind": "book_2"
-            },
-            {
-                "s_ind": "book_3"
-            }
-        ],
-        "key": "wordb"
+        "indexes": [{"s_ind": "book_2"}, {"s_ind": "book_3"}], "key": "wordb"
     };
 
     const response_3 = client.invoke('anezkasearch.IndexStorageService/InsertIndex', data_3);
@@ -61,11 +42,11 @@ export default () => {
     });
     console.log(response_3.message);
 
-    const response_4 = client.invoke('anezkasearch.SearchService/SearchIndex', { "text": "worda wordb" });
+    const response_4 = client.invoke('anezkasearch.SearchService/SearchIndex', {"text": "worda wordb"});
     check(response_4, {
         'status is OK 4': (r) => r && r.status === grpc.StatusOK,
-        'answer is correct 4':  response_4.message['indexes'][0]["sInd"] === "book_2"
-            &&  response_4.message['indexes'][1]["sInd"] === "book_3",
+        'answer is correct 4': response_4.message['indexes'][0]["sInd"] === "book_2"
+            && response_4.message['indexes'][1]["sInd"] === "book_3",
     });
     console.log(response_4.message);
 };
