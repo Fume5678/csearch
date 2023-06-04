049f6ef4ea4e989f5a5497690e85cbf7b56f4379
Nikita Selyavin <nselyavin@inbox.ru>
2023-06-04 22:00:12 +0300

    feat: created other benchmarks project

--- test/benchmarks/bench_Indexer.cpp
+++ test/benchmarks/bench_Indexer.cpp
@@ -5,60 +5,27 @@
 #include "../units/testmain.h"
 
 #ifdef ENABLED_BENCHMARK
-#include <IndexStorage.h>
 #include <catch2/benchmark/catch_benchmark.hpp>
 #include <chrono>
-#include <iostream>
-#include <random>
-#include <syncstream>
-#include <thread>
 #include <fstream>
+#include <iostream>
 
+#include <Indexer.h>
+#include <sources/PostgresSource.h>
+
+using namespace anezkasearch;
 using namespace std::chrono_literals;
 
-auto ReadWords(){
-  std::ifstream ifs("/home/fume/projects/cpp/anezkasearch/test/words.txt");
+TEST_CASE("Indexer benchmarks") {
+  cxxopts::Options options = GetOptions();
+  cxxopts::ParseResult res = options.parse(ARGC, ARGV);
+  YAML::Node config = YAML::LoadFile("./test_config.yaml");
+  auto state = std::make_shared<AppState<IntInd>>(res, config);
 
-  if(not ifs.is_open()){
-    throw std::runtime_error("words.txt file not found");
-  }
-  std::vector<std::string> words;
-
-  std::string word;
-  while(not ifs.eof()){
-    std::getline(ifs, word);
-    words.push_back(word);
-  }
-
-  ifs.close();
-  return words;
-}
-
-
-TEST_CASE("IndexStorage benchmark") {
-  // не ну а че
-  const std::vector<std::string> words = ReadWords();
-
-  std::random_device rd;
-  std::mt19937 gen(rd());  // Standard mersenne_twister_engine seeded with rd()
-
-  // For random keys
-  std::uniform_int_distribution<> distrib_keys(0, 1000);
-
-  // For random indexes
-  std::uniform_int_distribution<> distrib_indexes(1, 10000);
-  SECTION("One insert") {
-    anezkasearch::IndexStorage<anezkasearch::IntId> index_storage;
-
-    BENCHMARK("Sync insert") {
-      index_storage.Insert(words[distrib_keys(gen)], distrib_indexes(gen));
-    };
-
-    BENCHMARK("Sync get") {
-      index_storage.Get(words[distrib_keys(gen)]);
-    };
-  }
-
+  BENCHMARK("Postgres reading") {
+    Indexer<PostgresSource, IntInd> indexer(state);
+    indexer.Run();
+  };
 }
 
 #endif
